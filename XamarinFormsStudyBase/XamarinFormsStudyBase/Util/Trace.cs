336bd5e26f9e2d09bdc022acc39feb9a789b24db
makopy1980 <makopy.inside@gmail.com>
2017-03-07 11:37:09 +0900

    [Trace.cs]WriteNavigationStack, WriteModalStackを追加。コメント追記

--- XamarinFormsStudyBase/XamarinFormsStudyBase/Util/Trace.cs
+++ XamarinFormsStudyBase/XamarinFormsStudyBase/Util/Trace.cs
@@ -4,8 +4,14 @@ using System.Diagnostics;
 
 namespace Util
 {
+    /// <summary>
+    /// デバッグログ出力クラス
+    /// </summary>
     public static class Trace
     {
+        /// <summary>
+        /// ファイル名＋行番号＋メソッド名をログ出力
+        /// </summary>
         public static void WriteLine(string message,
                                      [CallerFilePath] string file = "",
                                      [CallerLineNumber] int line = 0,
@@ -15,6 +21,9 @@ namespace Util
             Debug.WriteLine(s);
         }
 
+        /// <summary>
+        /// ファイル名＋メソッド名をログ出力
+        /// </summary>
         public static void WriteMethod([CallerFilePath] string file = "",
                                        [CallerMemberName] string member = "")
         {
@@ -23,6 +32,36 @@ namespace Util
             var fileName = paths[paths.Length - 1];
 
             var s = string.Format("{0} - {1}", fileName, member);
+            Debug.WriteLine(s);
+        }
+
+        /// <summary>
+        /// NavigationStackをログ出力
+        /// </summary>
+        public static void WriteNavigationStack(Page currentPage)
+        {
+            var s = "***** NavigationStack *****\n";
+
+            foreach (var page in currentPage.Navigation.NavigationStack)
+            {
+                s += (page.ToString() + "\n");
+            }
+
+            Debug.WriteLine(s);
+        }
+
+        /// <summary>
+        /// ModalStackをログ出力
+        /// </summary>
+        public static void WriteModalStack(Page currentPage)
+        {
+            var s = "***** ModalStack *****\n";
+
+            foreach (var page in currentPage.Navigation.ModalStack)
+            {
+                s += (page.ToString() + "\n");
+            }
+
             Debug.WriteLine(s);
         }
     }
